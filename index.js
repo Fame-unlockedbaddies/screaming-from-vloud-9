e9e05956d62712bbc431c9cbe5bf18a0b5355fa0
Fame-unlockedbaddies <hoeniggerkys@proton.me>
2026-05-03 19:42:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,76 +34,59 @@ const client = new Client({
 // COMMAND
 const command = new SlashCommandBuilder()
   .setName("set")
-  .setDescription("Role panels")
-
-  // PART 1 (6 ROLES)
+  .setDescription("Role panel")
   .addSubcommand(sub =>
-    sub.setName("roles1")
-      .setDescription("First 6 roles")
-      .addStringOption(o => o.setName("title").setDescription("Title").setRequired(true))
-      .addStringOption(o => o.setName("background").setDescription("Image URL").setRequired(true))
+    sub
+      .setName("role-reactions")
+      .setDescription("Create role panel")
 
+      .addStringOption(o =>
+        o.setName("title").setDescription("Panel title").setRequired(true)
+      )
+      .addStringOption(o =>
+        o.setName("background").setDescription("Image URL").setRequired(true)
+      )
+
+      // 🔥 7 ROLE LIMIT (SAFE)
       .addRoleOption(o => o.setName("role1").setDescription("Role 1"))
       .addStringOption(o => o.setName("emoji1").setDescription("Emoji 1"))
-      .addStringOption(o => o.setName("name1").setDescription("Name 1"))
+      .addStringOption(o => o.setName("name1").setDescription("Button name 1"))
 
       .addRoleOption(o => o.setName("role2").setDescription("Role 2"))
       .addStringOption(o => o.setName("emoji2").setDescription("Emoji 2"))
-      .addStringOption(o => o.setName("name2").setDescription("Name 2"))
+      .addStringOption(o => o.setName("name2").setDescription("Button name 2"))
 
       .addRoleOption(o => o.setName("role3").setDescription("Role 3"))
       .addStringOption(o => o.setName("emoji3").setDescription("Emoji 3"))
-      .addStringOption(o => o.setName("name3").setDescription("Name 3"))
+      .addStringOption(o => o.setName("name3").setDescription("Button name 3"))
 
       .addRoleOption(o => o.setName("role4").setDescription("Role 4"))
       .addStringOption(o => o.setName("emoji4").setDescription("Emoji 4"))
-      .addStringOption(o => o.setName("name4").setDescription("Name 4"))
+      .addStringOption(o => o.setName("name4").setDescription("Button name 4"))
 
       .addRoleOption(o => o.setName("role5").setDescription("Role 5"))
       .addStringOption(o => o.setName("emoji5").setDescription("Emoji 5"))
-      .addStringOption(o => o.setName("name5").setDescription("Name 5"))
+      .addStringOption(o => o.setName("name5").setDescription("Button name 5"))
 
       .addRoleOption(o => o.setName("role6").setDescription("Role 6"))
       .addStringOption(o => o.setName("emoji6").setDescription("Emoji 6"))
-      .addStringOption(o => o.setName("name6").setDescription("Name 6"))
-  )
-
-  // PART 2 (5 ROLES)
-  .addSubcommand(sub =>
-    sub.setName("roles2")
-      .setDescription("Next 5 roles")
-      .addStringOption(o => o.setName("title").setDescription("Title").setRequired(true))
-      .addStringOption(o => o.setName("background").setDescription("Image URL").setRequired(true))
+      .addStringOption(o => o.setName("name6").setDescription("Button name 6"))
 
       .addRoleOption(o => o.setName("role7").setDescription("Role 7"))
       .addStringOption(o => o.setName("emoji7").setDescription("Emoji 7"))
-      .addStringOption(o => o.setName("name7").setDescription("Name 7"))
-
-      .addRoleOption(o => o.setName("role8").setDescription("Role 8"))
-      .addStringOption(o => o.setName("emoji8").setDescription("Emoji 8"))
-      .addStringOption(o => o.setName("name8").setDescription("Name 8"))
-
-      .addRoleOption(o => o.setName("role9").setDescription("Role 9"))
-      .addStringOption(o => o.setName("emoji9").setDescription("Emoji 9"))
-      .addStringOption(o => o.setName("name9").setDescription("Name 9"))
-
-      .addRoleOption(o => o.setName("role10").setDescription("Role 10"))
-      .addStringOption(o => o.setName("emoji10").setDescription("Emoji 10"))
-      .addStringOption(o => o.setName("name10").setDescription("Name 10"))
-
-      .addRoleOption(o => o.setName("role11").setDescription("Role 11"))
-      .addStringOption(o => o.setName("emoji11").setDescription("Emoji 11"))
-      .addStringOption(o => o.setName("name11").setDescription("Name 11"))
+      .addStringOption(o => o.setName("name7").setDescription("Button name 7"))
   );
 
 // REGISTER
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
+  console.log("Registering command...");
   await rest.put(
     Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
     { body: [command.toJSON()] }
   );
+  console.log("Done.");
 })();
 
 client.once("ready", () => console.log("Bot ready"));
@@ -111,8 +94,10 @@ client.once("ready", () => console.log("Bot ready"));
 // INTERACTIONS
 client.on(Events.InteractionCreate, async interaction => {
 
-  if (interaction.isChatInputCommand() && interaction.commandName === "set") {
-
+  if (
+    interaction.isChatInputCommand() &&
+    interaction.commandName === "set"
+  ) {
     await interaction.deferReply({ ephemeral: true });
 
     const title = interaction.options.getString("title");
@@ -120,8 +105,7 @@ client.on(Events.InteractionCreate, async interaction => {
 
     const roles = [];
 
-    // collect roles dynamically
-    for (let i = 1; i <= 11; i++) {
+    for (let i = 1; i <= 7; i++) {
       const role = interaction.options.getRole(`role${i}`);
       const emoji = interaction.options.getString(`emoji${i}`);
       const name = interaction.options.getString(`name${i}`);
@@ -165,7 +149,7 @@ client.on(Events.InteractionCreate, async interaction => {
       components: rows
     });
 
-    await interaction.editReply("Panel created");
+    await interaction.editReply("Panel created.");
   }
 
   // BUTTON HANDLER
@@ -175,10 +159,10 @@ client.on(Events.InteractionCreate, async interaction => {
 
     if (member.roles.cache.has(roleId)) {
       await member.roles.remove(roleId);
-      return interaction.reply({ content: "Removed", ephemeral: true });
+      return interaction.reply({ content: "Role removed", ephemeral: true });
     } else {
       await member.roles.add(roleId);
-      return interaction.reply({ content: "Added", ephemeral: true });
+      return interaction.reply({ content: "Role added", ephemeral: true });
     }
   }
 
